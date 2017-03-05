07b891b7043c36c00851bb7ba20117b1ae9d13a1
Benjamin <benjie.stevenson@gmail.com>
2017-03-05 16:14:40 -0500

    v1.2 CHANGELOG:
    -Continued work on player scripts

--- Assets/Scripts/Character/CharacterStats.cs
+++ Assets/Scripts/Character/CharacterStats.cs
@@ -8,6 +8,9 @@ public class CharacterStats : MonoBehaviour
     public int m_HealthPoints;
     public int m_DefensePower;
 
+    [Header("Target Info")]
+    public MoveableCharacter m_target;
+
     public int AP { get { return m_AttackPower; } }
     public int HP { get { return m_HealthPoints; } set { m_HealthPoints = value; } }
     public int DEF { get { return m_DefensePower; } }
@@ -16,8 +19,30 @@ public class CharacterStats : MonoBehaviour
 
     public void AttackTarget(CharacterStats other)
     {
-        if(other.HP > 0)
+        m_target = other.GetComponent<MoveableCharacter>();
+
+        if (other.HP > 0 && TargetInRange() )
         {
+        
+            if(this.name.Contains("Player"))
+            {
+                /*GameObject[] tempArray = new GameObject[EnemyCharacters.Instance.TeamSize - 1];
+                //GameObject tempGO;
+
+                for(int i = 0; i < tempArray.Length + 1; i++ )
+                {
+                    if(EnemyCharacters.Instance.Team[i])
+                    {
+                        
+                    }
+
+                    else
+                    {
+                        //tempArray = EnemyCharacters.Instance.Team[i].gameObject;
+                    }
+                }*/
+            }
+
             other.HP -= CalculateDamage(AP, other.DEF);
 
 
@@ -27,6 +52,7 @@ public class CharacterStats : MonoBehaviour
             if (other.HP <= 0)
             {
                 print(this.gameObject.name + " killed: " + other.gameObject.name);
+                //remove other.gameobject from array;
                 other.Kill();
             }
 
@@ -35,13 +61,15 @@ public class CharacterStats : MonoBehaviour
                 print(other.gameObject.name + " has " + other.HP + " HP remaining!");
             }
         }
+
+        m_target = null;
     }
 
     public void Kill()
     {
         //Play death animation if necessary
         //remove character from field;
-        Destroy(this.gameObject);
+        this.gameObject.SetActive(false);
     }
 
     private int CalculateDamage(int Attack, int EnemyDefense)
@@ -49,5 +77,21 @@ public class CharacterStats : MonoBehaviour
         return Attack - EnemyDefense;
     }
 
+    private bool TargetInRange()
+    {
+        Vector2 TargetLocIndex = m_target.m_CurrentLocation;
+        Vector2 AttackerLocIndex = this.GetComponent<MoveableCharacter>().m_CurrentLocation;
 
+        Vector2 TargetLocation = Map.Instance.GetPosition(TargetLocIndex.x, TargetLocIndex.y);
+        Vector2 AttackerLocation = Map.Instance.GetPosition(AttackerLocIndex.x, AttackerLocIndex.y);
+
+        if(TargetLocation.x >= AttackerLocation.x - 1) // - RANGE
+        {
+            //check to the left
+            //if above is true, target is withn range
+            return true;
+        }
+
+        return false;
+    }
 }
