a0a62fc1ce228d2370711894db527e40b09be81b
Astalto <gowaoe@gmail.com>
2017-03-09 09:45:48 -0500

    Testing commits with SourceTree

---
+++ Assets/SourceTreeTest.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SourceTreeTest : MonoBehaviour {
+    //No code, just a comment to prove that this is functional
+    //If you can read this, the sourcetree test was a success
+}
